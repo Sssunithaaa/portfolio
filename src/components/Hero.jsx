def0969562f51a349e1dc53eb8e5c6e2ca9188d9
Sunitha <108746093+Sssunithaaa@users.noreply.github.com>
2024-04-14 20:01:39 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,8 +37,8 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           {" "}
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+{/*           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" /> */}
         </div>
         <div className="flex flex-col">
           <motion.div variants={textVariantt(0.8)}>
